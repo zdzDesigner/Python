43be8b70d9e8d481e5671b0e6785bd79b8057cc1
zdzDesigner <1696498456@qq.com>
2025-10-26 22:34:47 +0800

    init

---
+++ http-server/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+# 中间件（类似 Koa）
+@app.middleware("http")
+async def add_request_id(request: Request, call_next):
+    request.state.request_id = "req-456"  # 类似 Koa 的 ctx.state
+    response = await call_next(request)
+    response.headers["X-Request-ID"] = request.state.request_id
+    return response
+
+
+# 路由
+@app.get("/")
+async def hello_world(request: Request):
+    return {"message": f"Hello {request.state.request_id}"}
+
+
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8100)
+    # uvicorn.run(app, host="0.0.0.0", port=8100, reload=True)  # reload=True 开发时自动重载
